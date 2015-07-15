d52644a75bd5bb038f2da2a9e72597f8b5b9cb0c
Modulus <thejohnskauge@gmail.com>
2015-07-15 12:15:17 +0200

    First commit

---
+++ MongoConsole2/Repo.cs
@@ -0,0 +1,92 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace MongoConsole2
+{
+    public class Repo
+    {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
+        public Repo()
+        {
+
+            _client = new MongoClient("mongodb://192.168.59.103:27017");
+            _database = _client.GetDatabase("test");
+        }
+
+        public async void insert()
+        {
+            var document = new BsonDocument
+            {
+                { "address" , new BsonDocument
+                    {
+                        { "street", "2 Avenue" },
+                        { "zipcode", "10075" },
+                        { "building", "1480" },
+                        { "coord", new BsonArray { 73.9557413, 40.7720266 } }
+                    }
+                },
+                { "borough", "Manhattan" },
+                { "cuisine", "Italian" },
+                { "grades", new BsonArray
+                    {
+                        new BsonDocument
+                        {
+                            { "date", new DateTime(2014, 10, 1, 0, 0, 0, DateTimeKind.Utc) },
+                            { "grade", "A" },
+                            { "score", 11 }
+                        },
+                        new BsonDocument
+                        {
+                            { "date", new DateTime(2014, 1, 6, 0, 0, 0, DateTimeKind.Utc) },
+                            { "grade", "B" },
+                            { "score", 17 }
+                        }
+                    }
+                },
+                { "name", "Vella" },
+                { "restaurant_id", "41704620" }
+            };
+
+            var collection = _database.GetCollection<BsonDocument>("restaurants");
+            await collection.InsertOneAsync(document);
+        }
+
+        public async Task<int> Query1()
+        {
+            var collection = _database.GetCollection<BsonDocument>("restaurants");
+            var filter = new BsonDocument();
+
+            var count = 0;
+            using (var cursor = await collection.FindAsync(filter))
+            {
+                while (await cursor.MoveNextAsync())
+                {
+                    var batch = cursor.Current;
+                    foreach (var document in batch)
+                    {
+                        // process document
+                        count++;
+                    }
+                }
+            }
+      
+
+            return count;
+        }
+
+        public async Task<List<BsonDocument>> Query2()
+        {
+            var collection = _database.GetCollection<BsonDocument>("restaurants");
+            var filter = Builders<BsonDocument>.Filter.Eq("borough", "Manhattan");
+            return await collection.Find(filter).ToListAsync();
+        }
+    }
+}
---
+++ MongoConsole2Tests/GettingStartedTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoConsole2;
+using Xunit;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using FluentAssertions;
+
+namespace MongoConsole2Tests
+{
+    public class GettingStartedTest
+    {
+
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
+        public GettingStartedTest()
+        {
+
+            _client = new MongoClient("mongodb://192.168.59.103:27017");
+            _database = _client.GetDatabase("test");
+        }
+
+        [Fact]
+        public async void Query1_Test(){
+            var repo = new Repo();
+            var result = await repo.Query1();
+
+            result.Should().BeGreaterOrEqualTo(25359);
+        }
+
+        [Fact]
+        public async void Query2_Test()
+        {
+                var repo = new Repo();
+                var result = await repo.Query2();
+
+                result.Should().NotBeEmpty();
+
+        }
+    }
+}
